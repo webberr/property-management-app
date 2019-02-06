5e2626d5b02619836f167a3f20e02c9dcc50c80a
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-06 16:37:50 +0100

    implemented second layout, updated navbar partials for both the landing page and application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+	layout :layout_by_resource
+
+	protected
+		def layout_by_resource
+			if devise_controller? && resource_name == :user && action_name == 'new'
+				"app"
+			else
+				"application"
+			end
+		end
 end
