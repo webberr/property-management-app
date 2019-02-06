5e8f8cab62e21cbbeeec61f7cb5405ecaa43cf6c
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-06 13:06:15 +0100

    landing page desing added

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,24 @@
+// NAVIGATION EFFECT
+$(function(){
+	$(window).scroll(function(){
+		if( $(window).scrollTop() >= 130 ){
+			$('.header-top')
+				.removeClass('navbar-fixed-top')
+				.css({transition: 'all 0.2s ease-in-out'})
+				.addClass('navbar-static-top');
+
+			$('.header_navbar')
+				.css("margin-top", "0")
+				.addClass('grey-nav-bg');
+		} else{
+			$('.header-top')
+				.removeClass('navbar-static-top')
+				.addClass('navbar-fixed-top')
+				.css({transition: 'all 0.2s ease-in-out'});
+
+			$('.header_navbar')
+				.removeClass('grey-nav-bg')
+				.css({marginTop: "8rem", transition: 'all 0.5s ease-in-out'});
+		};
+	});
+});
---
+++ app/controllers/home_page_controller.rb
@@ -0,0 +1,5 @@
+class HomePageController < ApplicationController
+	# layout "landing"
+  def index
+  end
+end
